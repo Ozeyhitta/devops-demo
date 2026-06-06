5480967ca5ace0d5b57720dfc4a6c2c2b741980f
Ozeyhitta <mkhang4444@gmail.com>
2026-06-06 20:24:44 +0700

    fix: add gitignore

---
+++ app.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('./app');
+
+test('GET / trả về Hello World', async () => {
+  const res = await request(app).get('/');
+  expect(res.statusCode).toBe(200);
+  expect(res.body.message).toBe('Hello World!');
+});
---
+++ server.js
@@ -0,0 +1,2 @@
+const app = require('./app');
+app.listen(3000, () => console.log('Chạy trên port 3000'));
