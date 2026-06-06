26f2672a9dc0f8689f102eb21b8209d9c44f24b6
Ozeyhitta <mkhang4444@gmail.com>
2026-06-06 21:55:25 +0700

    feat: update message and fix test

--- app.test.js
+++ app.test.js
@@ -4,5 +4,5 @@ const app = require('./app');
 test('GET / trả về Hello World', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
-  expect(res.body.message).toBe('Hello World! 🚀');
+expect(res.body.message).toBe('Hello từ Kubernetes! 🚀');
 });
