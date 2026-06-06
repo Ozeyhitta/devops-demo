4f35a511f748f3ac657c7698b6292c086d8e863a
Ozeyhitta <mkhang4444@gmail.com>
2026-06-06 21:53:59 +0700

    feat: add version and pod name

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World! 🚀' });
+  res.json({ 
+    message: 'Hello từ Kubernetes! 🚀',
+    version: '2.0',
+    pod: process.env.HOSTNAME
+  });
 });
 
 module.exports = app;
